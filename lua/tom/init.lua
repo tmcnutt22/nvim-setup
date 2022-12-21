890b0d34d0650707a970637527e453ef10526949
tom m <thomas.mcnutt.jr@gmail.com>
2022-12-20 22:58:36 -0500

    first commit

---
+++ lua/tom/init.lua
@@ -0,0 +1,10 @@
+require("tom.remap")
+require("tom.set")
+
+-- colorscheme
+require('onedark').setup {
+	style = 'warmer'
+}
+require('onedark').load()
+
+
---
+++ lua/tom/set.lua
@@ -0,0 +1,28 @@
+-- line numbering
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+--tabs
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+--colors
+vim.opt.termguicolors = true
+
+--scroll
+vim.opt.scrolloff = 8
+
+--column
+vim.opt.colorcolumn = "80"
+
+--leader key
+vim.g.mapleader = " "
+
+--update time
+vim.opt.updatetime = 50
